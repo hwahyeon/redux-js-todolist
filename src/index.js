c595cd4337c5c9f08760cfcb468e3ce8066de3c7
hwahyeon <hwahyeon@knou.ac.kr>
2023-09-20 03:54:23 +0900

    Resolving Vanilla JavaScript and Readerx Interrelationships Errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+import { createStore } from "redux";
 
-console.log(add)
+document.addEventListener('DOMContentLoaded', () => {
+  const add = document.getElementById("add");
+  const minus = document.getElementById("minus");
+  const number = document.querySelector('#numberSpan');
+  number.innerText = '0';
+  
 
-let count = 0;
-number.innerText = count;
+  const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+      return count + 1;
+    } else if (action.type === "MINUS") {
+      return count - 1;
+    } else {
+      return count;
+    }
+  };
+  const countStore = createStore(countModifier);
+  
+  const onChange = () => {
+    number.innerText = countStore.getState();
+  };
+  
+  countStore.subscribe(onChange);
+  
+  const handleAdd = () => {
+    countStore.dispatch({ type: "ADD" });
+  };
+  
+  const handleMinus = () => {
+    countStore.dispatch({ type: "MINUS" });
+  };
+  
+  add.addEventListener("click", handleAdd);
+  minus.addEventListener("click", handleMinus);
+});
 
-const updateText = () => {
-  number.innerText = count;
-};
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
